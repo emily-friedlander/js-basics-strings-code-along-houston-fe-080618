38ea7f37286e032371842531c2f37be10b41c38b
Emily Friedlander <emily@emilyfriedlander.com>
2018-09-09 20:30:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const currentUser = 'Emily';
 // const welcomeMessage = 'Welcome to Flatbook, ';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
